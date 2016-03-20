d6bff7ea2a5db4b37aa3afb89e614282cedeb1ef
ampe <ampe@localhost.localdomain>
2016-03-20 10:39:58 +0100

    updated awesome theme

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -145,7 +145,7 @@ local layouts = {
 
 	-- tags
 local tags = {
-	names = {"1", "2", "3", "4", "5", "6", "7", "8"}
+	names = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 }
 for i = 1, screen.count() do
 	tags[i] = awful.tag( tags.names, i, layouts[1] )
