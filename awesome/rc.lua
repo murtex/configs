78dde204021d283d795e3d9267aac3832895c27f
Stephan R. Kuberski <kuberski@nui-potsdam.de>
2016-10-17 08:38:36 +0200

    updated configs

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -150,7 +150,7 @@ local layouts = {
 
 	-- tags
 local tags = {
-	names = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	names = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 }
 for i = 1, screen.count() do
 	tags[i] = awful.tag( tags.names, i, layouts[1] )
